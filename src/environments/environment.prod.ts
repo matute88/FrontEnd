c81e82314bfb5a731a9e287db6d5f3c5103e141b
matute88 <100228107+matute88@users.noreply.github.com>
2022-10-22 00:11:36 -0300

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontendmjl',
+    appId: '1:700859890164:web:f8d5d2818b104c2942a1a1',
+    storageBucket: 'frontendmjl.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyCe3A4zOvtC1U8rW8-M2LGKwd4MJheAegs',
+    authDomain: 'frontendmjl.firebaseapp.com',
+    messagingSenderId: '700859890164',
+  },
+  production: true,
+  //URL: 'http://localhost:4200'
+  URL: 'https://backendmjl.herokuapp.com/'
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  URL: 'http://localhost:4200'
+  //URL: 'https://backendmjl.herokuapp.com/'
+};
